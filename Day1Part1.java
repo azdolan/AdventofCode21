90b8ebe1fd40a7e65c424d3029b3e69d8537d2e7
azdolan <55203039+azdolan@users.noreply.github.com>
2021-12-01 21:11:04 +0000

    Create Day1Part1.java

---
+++ Day1Part1.java
@@ -0,0 +1,30 @@
+import java.io.File;
+        import java.util.ArrayList;
+        import java.util.Scanner;
+
+public class Day1 {
+
+    public static void main(String[] args) throws Exception {
+        ArrayList<Integer> measures= new ArrayList<Integer>();
+        File input = new File("/Users/aidandolan/Desktop/AdventofCode21/src/day1.txt");
+        Scanner sc = new Scanner(input);
+        int count = 0;
+        int increases = 0;
+
+        while (sc.hasNextLine()){
+            int nextMeasure = Integer.parseInt(sc.nextLine());
+            measures.add(nextMeasure);
+            count++;
+        }
+
+        for (int i=0; i<count-1; i++){
+            if (measures.get(i) < measures.get(i+1)){
+                increases++;
+            }
+        }
+
+        System.out.println(increases);
+
+    }
+
+}
